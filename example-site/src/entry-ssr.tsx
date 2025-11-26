5cd2af45d6184778a6d910f426a055f354ee5adb
Nate Buckareff <nate.buckareff@gmail.com>
2025-11-25 21:26:56 -0800

    Cleanup unused files

--- example-site/src/entry-ssr.tsx
+++
@@ -1,9 +0,0 @@
-// src/entry-ssr.tsx
-import { renderToString, generateHydrationScript } from 'solid-js/web';
-import { App } from './app';
-
-export function renderIslandApp() {
-  const html = renderToString(() => <App />); // renders loading state if you use Suspense
-  const hydrationScript = generateHydrationScript(); // global script, used once per page
-  return { html, hydrationScript };
-}
